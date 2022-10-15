6ac0b79f9dbb0a5c1db682b9c4bcbb95fe87b484
awanocode <111249691+awanocode@users.noreply.github.com>
2022-10-14 21:10:18 -0300

    first shitpost
    abubublé

---
+++ js/script1.js
@@ -0,0 +1,43 @@
+/* Iniciando os trabalhos
+Requisitos:
+
+3 estruturas condicionais (2,5)
+1 laço de repetição com outro dentro (3)
+uso de vetores (2,5)
+entradas e saídas  com HTML (1)
+Apresentação em 18/10 (1)
+
+Bônus uso de FrameWork CSS (1)
+Entrega no GIT
+Limite de entrega 19/10 */
+
+const dino = document.getElementById("dino");
+const cactus = document.getElementById("cactus");
+function jump() {
+    if (dispatchEvent.classList != "jump") {
+        //first it checks if the dino is mid-jump. If not, it makes it jump.
+        dino.classList.add("jump");
+        setTimeout(function () {
+            dino.classList.remove("jump");
+            //removes the jump class from the dino once it has jumped so that it can jump again
+        }, 300);
+    }
+}
+let checkAlive = setInterval(function () {
+    let dinoTop = parseInt(
+        window.getComputedStyle(dino).getPropertyValue("top")
+    );
+    let cactusLeft = parseInt(
+        window.getComputedStyle(cactus).getPropertyValue("left")
+    );
+    //check for collision
+    if (cactusLeft > 0 && cactusLeft < 70 && dinoTop >= 143) {
+        dino.style.animationPlayState = "paused";
+        cactus.style.animationPlayState = "paused";
+        alert("Whoops! Game Over :(");
+        window.location.reload();
+    }
+}, 10);
+document.addEventListener("keydown", function (event) {
+    jump();
+});
